9c11e30eb213524688e7b1b71e8afd4d22d94d71
lss233 <m18760286457@163.com>
2018-01-07 12:38:20 +0800

    Update SpigotUtils, PhoenixUtils
    Signed-off-by: lss233 <m18760286457@163.com>

---
+++ PhoenixForSpigot/src/main/java/com/lss233/phoenix/spigot/PhoenixUtils.java
@@ -0,0 +1,30 @@
+package com.lss233.phoenix.spigot;
+
+import com.lss233.phoenix.entity.Entity;
+import com.lss233.phoenix.world.Location;
+import org.bukkit.World;
+
+import static org.bukkit.Bukkit.*;
+
+/**
+ *
+ */
+public class PhoenixUtils {
+    /**
+     * Covert a Phoenix Entity instance to a Spigot instance
+     * @param entity The Phoenix Entity
+     * @return The Spigot Entity
+     */
+    public static org.bukkit.entity.Entity toSpigot(Entity entity) {
+        return getEntity(entity.getUniqueId());
+    }
+
+    /**
+     * Covert a Phoenix Location instance to a Spigot instance
+     * @param location The Phoenix Location
+     * @return The Spigot Location
+     */
+    public static org.bukkit.Location toSpigot(Location location) {
+        return new org.bukkit.Location(getWorld(location.getWorld().getUniqueId()), location.getX(), location.getY(), location.getZ());
+    }
+}
